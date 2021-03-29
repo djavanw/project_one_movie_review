394a5113f391c91467f6e6a17025a71775b07e01
Javan <71285290+djavanw@users.noreply.github.com>
2021-03-28 18:09:52 -0600

    Got rid of the form/submit and went back to the click.  The button got messed up. Probably something to do with Bulma.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const movieApiKey = "f3192513";
 //Reminder, below line is pure JavaScript
 var searchInputEl = document.querySelector("#searchInput");
-var formSearchEl = document.querySelector("#formSearch"); 
-var searchBtnEl = $("#searchBtn");
+// var formSearchEl = document.querySelector("#formSearch"); 
 var searchNewBtnEl = document.querySelector("#searchBtn");
+// var searchBtnEl = $("#searchBtn");
 var thumbnailEl = $("#thumbnail");
 var yearEl = $("#year");
 var titleEl = $("#title");
@@ -126,7 +126,7 @@ function pullStorageMovie() {
 pullStorageMovie();
 
 // searchBtnEl.on("click", searching);
-formSearchEl.addEventListener("submit", function(event) {
+searchNewBtnEl.addEventListener("click", function(event) {
     event.preventDefault();
     searching(event, searchInputEl.value);
 });
